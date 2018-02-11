4a63c6c1fd845beaa00e1ec8d5ad1dc88d4d609b
k.hirata <k.hirata@prokhirataqloog.local>
2018-02-11 23:19:45 +0900

    It can be executed using arguments

--- noa_sele_bs4.py
+++ noa_sele_bs4.py
@@ -4,6 +4,11 @@
 import selenium.webdriver
 import bs4
 import pprint
+import sys
+import re
+
+rservation_day = sys.argv[1]
+rservation_time = sys.argv[2]
 
 username = "0095848"
 password = "1112"
@@ -19,56 +24,62 @@ login_username.send_keys(username)
 login_password = browser.find_element_by_name("f_password")
 login_password.send_keys(password)
 login_password.send_keys('\n')
+
 #振替予約の画面を表示
 browser.find_element_by_css_selector('[href="./reserv/reserv_date.php"]').click()
+
 #予約日程を指定
-browser.execute_script("javascript:go_next('2018-02-05')")
+javascript_rservation_day  = "javascript:go_next('" + rservation_day + "')"
+browser.execute_script(javascript_rservation_day)
 
 #BeautifulSoupで対象の時間の予約ボタンを取得
 #テーブル全体を取得
 html = browser.page_source
-soup = bs4.BeautifulSoup(html)
+soup = bs4.BeautifulSoup(html, "html.parser")
 tr = soup.select('tr')
 count=0
 result_item = ""
 for item_bs4 in tr:
     item = str(item_bs4)
     count+=1
-    if "横田" in item:
+    pattern = re.compile(r"%s" % rservation_time)
+    match = pattern.search(item)
+    if match is None:
+        result = False
+    else:
         result_item = item
         result = True
         break
-    else:
-        result = False
-#print(result)
-#print(count)
-#print('---')
-#print(result_item)
-#print('---')
+
 split_item = result_item.split('<tr')
-#pprint.pprint(split_item)
 
 #対象のレコードのみ取得
 count2 = 0
+result_item2 = ""
 for item2 in split_item:
     count2 += 1
-    if "横田" in item2:
+    pattern2 = re.compile(r"%s" % rservation_time)
+    match2 = pattern.search(item2)
+    if match2 is None:
+        result2 = False
+    else:
         result_item2 = item2
         result2 = True
         break
-    else:
-        result2 = False
-#print(result2)
-#print(count2)
-#print('---')
-#print(result_item2)
-#print('---')
+
+
+if result2:
+    print('空いてます')
+else:
+    print('満員でした')
+    sys.exit()
+
 
 #対象のHTML行のみ取得
 splitline_item2 = result_item2.splitlines()
-#pprint.pprint(splitline_item2)
 
 count3 = 0
+result_item3 = ""
 for item3 in splitline_item2:
     count3 += 1
     if "javascript" in item3:
@@ -78,17 +89,13 @@ for item3 in splitline_item2:
     else:
         result3 = False
         print('満員でした')
-#print(result3)
-#print(count3)
-#print('---')
-#pprint.pprint(result_item3)
-#print('---')
+        sys.exit()
 
 #対象のjavascript文のみ取得
 splitdq_item3 = result_item3.split('\"')
-#pprint.pprint(splitdq_item3)
 
 count4 = 0
+result_item4 = ""
 for item4 in splitdq_item3:
     count4 += 1
     if "javascript" in item4:
@@ -97,24 +104,13 @@ for item4 in splitdq_item3:
         break
     else:
         result4 = False
-print(result4)
-print(count4)
-print('---')
-pprint.pprint(result_item4)
-print('---')
-print(type(result_item4))
+
+if result4 == 'False':
+    sys.exit()
 
 #予約ボタンクリック
 browser.execute_script(result_item4)
 
-#参考
-#browser.execute_script("javascript:go_next('2018-02-05')")
-
 #予約確認ボタンクリック
 browser.find_element_by_name("送　信").click()
-
-
-
-
-
-
+print('予約できました。')
